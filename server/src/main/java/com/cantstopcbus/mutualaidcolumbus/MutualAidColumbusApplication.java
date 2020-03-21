7b9c2951dcd16b9af908a825c691f12a37dfa88e
Eddie <eddie@deeplens.ai>
2020-03-21 17:21:46 -0400

    Spring Boot Angular starter

---
+++ server/src/main/java/com/cantstopcbus/mutualaidcolumbus/MutualAidColumbusApplication.java
@@ -0,0 +1,13 @@
+package com.cantstopcbus.mutualaidcolumbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutualAidColumbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutualAidColumbusApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/cantstopcbus/mutualaidcolumbus/MutualAidColumbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cantstopcbus.mutualaidcolumbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutualAidColumbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
